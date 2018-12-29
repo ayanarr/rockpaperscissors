d7ce1c3c77bde2585cc94178ee8df972a52a4ec4
ayana <ayanaroman84@gmail.com>
2018-12-29 01:22:32 +0000

    updated java

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@
 /*  global $*/
 
 $("#shoot").click(function(){
-     var tie
-     var computerChoice 
+     var winner;
+     var computerChoice;
      var userChoice = $("#input").val();
      $("#userChoice").text(userChoice);
      var randomNumber = Math.random();
@@ -15,23 +15,31 @@ $("#shoot").click(function(){
          computerChoice="rock";
      } else if(randomNumber>0.333 && randomNumber<0.666){
           computerChoice="scissors"; 
-     } else{
-          computerChoice="paper";
+     } else
+        {computerChoice="paper";
      }
      $("#computerChoice").text(computerChoice);
      
      if(userChoice==="rock" && computerChoice==="paper"){
-          var winner="computer wins";
+           winner="computer wins";
      }else if (userChoice==="paper" && computerChoice==="rock"){
           winner="user wins";
+     }else if (userChoice==="scissors" && computerChoice==="paper"){
+          winner="user wins";
+     }else if (userChoice==="paper" && computerChoice==="scissors"){
+          winner="computer wins";
      } else if (userChoice==="paper" && computerChoice==="paper"){
-          var tie ="computer and user lose";
+          winner="computer and user lose";
      }else if (userChoice==="rock" && computerChoice==="rock"){
-           tie ="computer and user lose";
-          
-     $("#result").text(winner)();
-     }
-       $("#result").text(tie)();
+           winner ="computer and user lose";
+     }else if (userChoice==="rock" && computerChoice==="scissors"){
+          winner="user wins";
+     }else if (userChoice==="scissors" && computerChoice==="rock"){
+          winner="computer wins";
+     }else if (userChoice==="scissors" && computerChoice==="scissors"){
+          winner="computer and user lose";
+}
+ $("#result").text(winner);
 });
 
 
